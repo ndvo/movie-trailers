26dc1fba18b54555c11dd92d7c3e8c02e5e49795
ndvo <nelson@ocastudios.com>
2018-12-24 18:04:24 -0200

    Documentation for main.py

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from request_handler import RequestHandler
 from movie_collection import Collection
 from response import Response
 
-addr = ('', 8080)
-server = BaseHTTPServer.HTTPServer(addr, RequestHandler)
+# Create a nickname for os.paht.join
+j = os.path.join
+# Create the server
+server = BaseHTTPServer.HTTPServer(('', 8080), RequestHandler)
 
 collection = Collection()
-
-j = os.path.join
 
 for movie in movie_data.movies:
     collection.add_movie(movie)
 
 
 def main_page():
+    """
+    Create the main page 
+
+    Returns a Response Object with a list of movies sorted by title.
+    """
     css = open(j('styles', 'main.css')).read()
     js = open(j('scripts', 'main.js')).read()
     movie_page = page.Component('page', {'styles': css, 'js': js},  'page.html')
@@ -34,14 +39,26 @@ def main_page():
 
 
 def movie_page():
+    """
+    Create the Movie Page
+
+    Returns a Response Object with further details of a specific movie
+    """
     movie_page = page.Component()
     return Response(body=movie_page.render())
 
+def create_routes():
+    """
+    Creates routes within RequestHandler static attribute routes
 
-RequestHandler.routes[r'/'] = main_page
-RequestHandler.routes[r'/movie/\w+'] = movie_page
+    Each route is a key, value pair. The key is a regular expression and the
+    value is a function to be invoked by the RequestHandler class.
+    """
+    RequestHandler.routes[r'/'] = main_page
+    RequestHandler.routes[r'/movie/\w+'] = movie_page
+
 
 print "*** Movie Website running ***"
 print "    http://localhost:8080    "
-
+create_routes()
 server.serve_forever()
