fefc1540da592d1996245b41096e421a71ab4139
ndvo <nelson@ocastudios.com>
2018-12-24 18:10:41 -0200

    linting media.py

--- media.py
+++ media.py
@@ -3,7 +3,13 @@ class Movie():
     """"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(
+            self,
+            movie_title,
+            movie_storyline,
+            poster_image,
+            trailer_youtube
+            ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -15,6 +21,3 @@ class Movie():
 
     def __getitem__(self, key):
         return self.__dict__[key]
-
-
-
